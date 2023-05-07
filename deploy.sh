2a7f0273fb238791fa5eaa0bc62f27d94856f62e
John Feminella <jxf@jxf.me>
2023-05-06 22:20:00 -0400

    Install aws-cli during deployments and add bin to $PATH

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,13 @@
 # Install Task to `./bin`.
 sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d
 
+# Add ~/bin to path
+PATH=/opt/buildhome/bin:$PATH
+
+# Install aws-cli.
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install -i ~/aws-cli -b ~/bin
+
 # Run build task.
 ./bin/task build
