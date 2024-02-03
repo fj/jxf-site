b006a0c12796a5eeaa3176787a0514e2a7589c59
John Feminella <jxf@jxf.me>
2024-02-02 22:59:20 -0500

    Modify Taskfile to use AWS env var names that aren't Netlify-reserved

--- deploy.sh
+++ deploy.sh
@@ -11,5 +11,15 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 unzip awscliv2.zip
 ./aws/install -i ~/aws-cli -b ~/bin
 
+printenv
+
+echo "AWS access key is: [$SECRETS_AWS_SECRET_ACCESS_KEY]"
+test -z "$SECRETS_AWS_SECRET_ACCESS_KEY"
+echo "Exit code: $?"
+
+echo "AWS secret key is: [$SECRETS_AWS_ACCESS_KEY_ID]"
+test -z "$SECRETS_AWS_ACCESS_KEY_ID"
+echo "Exit code: $?"
+
 # Run build task.
 ./bin/task build
